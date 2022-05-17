9678f22dda8de758cf9b02e932ff19a21f00f360
Jakub Raczyński <95698623+jraczynski1409@users.noreply.github.com>
2022-05-17 02:32:30 +0200

    Update toggleMenu.js

--- scripts/toggleMenu.js
+++ scripts/toggleMenu.js
@@ -1,24 +1,19 @@
-var menu_icon = true;
+var menu_icon = false;
 
 function toggleMenu()
 {
     var element = document.getElementById("menu");
     
-    var button = document.getElementById("ikona_w_menu");
     
     if(menu_icon)
     {
-        button.src="/icons/menu_icon.png";
         element.removeAttribute("class","wyswietlaj");
         element.setAttribute("class","nie_wyswietlaj");
-        menu_icon=false;
     }
     else
     {
-        button.src="/icons/exit_menu_icon.png";
         element.setAttribute("class","wyswietlaj");
         element.removeAttribute("class","nie_wyswietlaj");
-        menu_icon=true;
     }
         
 }
